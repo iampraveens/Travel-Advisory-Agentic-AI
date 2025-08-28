3204e4cdcad8bad6912ec1eb2854237c8287f093
iampraveens <praveensivaprakasham@gmail.com>
2025-08-29 01:55:45 +0530

    Project Scaffolding - Template

---
+++ template.sh
@@ -0,0 +1,34 @@
+mkdir -p agent
+mkdir -p config
+mkdir -p research
+mkdir -p prompts
+mkdir -p tools
+mkdir -p utils
+
+touch agent/__init__.py
+touch agent/agentic_workflow.py
+touch config/__init__.py
+touch config/config.yaml
+touch research/experiments.ipynb
+touch research/trials.ipynb
+touch prompts/__init__.py
+touch prompts/prompt_template.py
+touch tools/__init__.py
+touch tools/arthmetic_ops_tool.py
+touch tools/currency_conversion_tool.py
+touch tools/expense_calculation_tool.py
+touch tools/place_search_tool.py
+touch tools/weather_info_tool.py
+touch utils/__init__.py
+touch utils/config_loader.py
+touch utils/currency_converter.py
+touch utils/expense_calculator.py
+touch utils/model_loader.py
+touch utils/place_info_searcher.py
+touch utils/weather_info.py
+touch utils/save_document.py
+touch .env 
+touch main.py
+touch app.py 
+touch requirements.txt 
+touch setup.py
