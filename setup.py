01a9e818da31a47d6a09e9e9d8ced2d9c3b87ba8
iampraveens <praveensivaprakasham@gmail.com>
2025-08-29 02:03:29 +0530

    Implemented Setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Travel-Advisory-Agent",
+    version="0.1.0",
+    packages=find_packages(),
+    author="iampraveens",
+    author_email="praveensivaprakasham@gmail.com",
+    description="An agentic AI system for personalized travel planning and advisory",
+    install_requires=[]
+)
